a442ccce8cae84ed1040b38f6919b376d77bdf7c
Hioda <muyi0053@gmail.com>
2022-03-15 16:54:33 +0800

    添加hello

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    for(int i=0; i<10; i++){
+        vec.push_back(i);
+    }
+    cout << *(vec.end()-1) << endl;
+    return 0;
+}
